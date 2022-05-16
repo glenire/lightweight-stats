d9c4d6cf84658a0ef070fe70ef856f59f8b3729e
dnichol_13 <admin@glenire.co.uk>
2022-05-16 11:58:02 +0100

    Initial commit

---
+++ Lightweight-Stats/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Lightweight_Stats;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Lightweight-Stats/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Lightweight_Stats;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
